1f293c1896935d30f0123f95890eb5c453d49c58
xkhaliil <ltaief.khalil@gmail.com>
2023-11-21 17:55:29 +0100

    login and splash screen auto route to home activity

--- app/src/main/java/com/example/renthub/Views/LoginActivity.java
+++ app/src/main/java/com/example/renthub/Views/LoginActivity.java
@@ -1,25 +1,69 @@
 package com.example.renthub.Views;
 
+import static android.service.controls.ControlsProviderService.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Pair;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.airbnb.lottie.LottieAnimationView;
 import com.example.renthub.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
-Button callSignup, login_btn,forgetpassword;
-TextView Welcometext,sloganwelcome;
-LottieAnimationView lottieanimation;
-TextInputLayout username,password;
+    private FirebaseAuth mAuth;
+    Button callSignup, login_btn, forgetpassword;
+    TextView Welcometext, sloganwelcome;
+    LottieAnimationView lottieanimation;
+    TextInputLayout username, password;
 
+    private boolean validateEmail() {
+        String val = username.getEditText().getText().toString().trim();
+        String checkspaces = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+        if (val.isEmpty()) {
+            username.setError("Field can not be empty");
+            return false;
+        } else if (!val.matches(checkspaces)) {
+            username.setError("Invalid Email!");
+            return false;
+        } else {
+            username.setError(null);
+            username.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    private boolean validatePassword() {
+        String val = password.getEditText().getText().toString().trim();
+
+        if (val.isEmpty()) {
+            password.setError("Field can not be empty");
+            return false;
+
+        }
+        else {
+            password.setError(null);
+            password.setErrorEnabled(false);
+            return true;
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mAuth = FirebaseAuth.getInstance();
         super.onCreate(savedInstanceState);
         getWindow().setFlags(android.view.WindowManager.LayoutParams.FLAG_FULLSCREEN, android.view.WindowManager.LayoutParams.FLAG_FULLSCREEN); // to hide the status bar
         setContentView(R.layout.activity_login);
@@ -31,6 +75,7 @@ TextInputLayout username,password;
         forgetpassword = findViewById(R.id.buttonforgetpassword);
         login_btn = findViewById(R.id.buttonlogin);
         callSignup = findViewById(R.id.buttonsignup);
+
         callSignup.setOnClickListener(new android.view.View.OnClickListener() {
             @Override
             public void onClick(android.view.View v) {
@@ -63,6 +108,34 @@ TextInputLayout username,password;
                 ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(LoginActivity.this, pairs);
                 startActivity(intent, options.toBundle());
 
+            }
+        });
+        login_btn.setOnClickListener(new android.view.View.OnClickListener() {
+
+            @Override
+            public void onClick(android.view.View v) {
+                if(!validatePassword()|!validateEmail()){
+                    return;
+                }
+                mAuth.signInWithEmailAndPassword(username.getEditText().getText().toString(), password.getEditText().getText().toString())
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d(TAG, "signInWithEmail:success");
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    Intent j = new Intent(LoginActivity.this, HomeActivity.class);
+                                    j.putExtra("user", user);
+                                    startActivity(j);
+                                    finish();
+                                } else {
+                                    Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                    Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
             }
         });
 
--- app/src/main/java/com/example/renthub/Views/SplashScreenActivity.java
+++ app/src/main/java/com/example/renthub/Views/SplashScreenActivity.java
@@ -3,7 +3,9 @@ package com.example.renthub.Views;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Pair;
 import android.view.WindowManager;
 import android.view.animation.Animation;
@@ -13,9 +15,11 @@ import android.widget.TextView;
 
 import com.airbnb.lottie.LottieAnimationView;
 import com.example.renthub.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SplashScreenActivity extends AppCompatActivity {
-
+    private FirebaseAuth mAuth;
     private static int SPLASH_SCREEN = 5000; // 5 seconds
 
     // variables
@@ -25,6 +29,7 @@ public class SplashScreenActivity extends AppCompatActivity {
     TextView slogan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mAuth = FirebaseAuth.getInstance();
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN); // to hide the status bar
         setContentView(R.layout.activity_splashscreen);
@@ -42,20 +47,37 @@ public class SplashScreenActivity extends AppCompatActivity {
         logo.setAnimation(bottomanim);
         slogan.setAnimation(bottomanim);
 
-        // to start the main activity after the splash screen
-        new android.os.Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                android.content.Intent intent = new android.content.Intent(SplashScreenActivity.this, LoginActivity.class);
-                Pair[] pairs = new Pair[3];
-                pairs[0] = new Pair<android.view.View, String>(car, "lottie_img");
-                pairs[1] = new Pair<android.view.View, String>(logo, "logo_img");
-                pairs[2] = new Pair<android.view.View, String>(slogan, "text_img");
-                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(SplashScreenActivity.this, pairs);
-                startActivity(intent, options.toBundle());
-                finish();
-            }
-        }, SPLASH_SCREEN);
 
+        // to start the main activity after the splash screen
+
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            new android.os.Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    android.content.Intent intent = new android.content.Intent(SplashScreenActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                }
+            }, SPLASH_SCREEN);
+        }else {
+            new android.os.Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    android.content.Intent intent = new android.content.Intent(SplashScreenActivity.this, LoginActivity.class);
+                    Pair[] pairs = new Pair[3];
+                    pairs[0] = new Pair<android.view.View, String>(car, "lottie_img");
+                    pairs[1] = new Pair<android.view.View, String>(logo, "logo_img");
+                    pairs[2] = new Pair<android.view.View, String>(slogan, "text_img");
+                    ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(SplashScreenActivity.this, pairs);
+                    startActivity(intent, options.toBundle());
+                    finish();
+                }
+            }, SPLASH_SCREEN);
+        }
     }
 }   
