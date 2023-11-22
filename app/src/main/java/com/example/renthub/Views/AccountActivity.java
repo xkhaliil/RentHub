508a1f1a89a67343ce66755d8d08531222f5757a
xkhaliil <ltaief.khalil@gmail.com>
2023-11-22 23:37:02 +0100

    location is now working

--- app/src/main/java/com/example/renthub/Views/AccountActivity.java
+++ app/src/main/java/com/example/renthub/Views/AccountActivity.java
@@ -1,17 +1,38 @@
 package com.example.renthub.Views;
 
+import static com.example.renthub.Views.SplashScreenActivity.area;
+import static com.example.renthub.Views.SplashScreenActivity.city;
+import static com.example.renthub.Views.SplashScreenActivity.country;
+import static com.google.android.gms.location.LocationServices.getFusedLocationProviderClient;
+import com.google.android.gms.maps.model.LatLng;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.os.Looper;
 import android.text.TextUtils;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.renthub.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -19,15 +40,16 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class AccountActivity extends AppCompatActivity {
+
     TextView CurrentLocation;
     RelativeLayout assistant;
     TextInputLayout username;
     TextInputLayout email;
     TextInputLayout phone;
-    TextInputLayout password;
     Button changePassword;
     ImageButton signOut;
     Button update;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +62,7 @@ public class AccountActivity extends AppCompatActivity {
         username = findViewById(R.id.usernameaccount);
         email = findViewById(R.id.emailAccount);
         phone = findViewById(R.id.phoneaccount);
-        password = findViewById(R.id.passwordaccount);
+
         signOut = findViewById(R.id.signoutbutton);
         changePassword = findViewById(R.id.changepasswordbutton);
         update = findViewById(R.id.updatebuttonaccount);
@@ -58,7 +80,7 @@ public class AccountActivity extends AppCompatActivity {
                     android.util.Log.d("TAG", "DocumentSnapshot data: " + task.getResult().getData());
                     username.getEditText().setText(task.getResult().getData().get("username").toString());
                     phone.getEditText().setText(task.getResult().getData().get("phone").toString());
-                    password.getEditText().setText(task.getResult().getData().get("password").toString());
+
                 } else {
                     android.util.Log.d("TAG", "get failed with ", task.getException());
                 }
@@ -81,8 +103,7 @@ public class AccountActivity extends AppCompatActivity {
                 washingtonRef
                         .update(
                                 "username", username.getEditText().getText().toString(),
-                                "phone", phone.getEditText().getText().toString(),
-                                "password", password.getEditText().getText().toString()
+                                "phone", phone.getEditText().getText().toString()
                         )
                         .addOnSuccessListener(aVoid -> {
                             Log.d("TAG", "DocumentSnapshot successfully updated!");
@@ -99,8 +120,34 @@ public class AccountActivity extends AppCompatActivity {
                 showAlertDialog("Invalid Input", "Please enter valid information.");
             }
         });
+        // Get the current location of the device
+        String FullAddress = area+" "+city+" "+country;
+        CurrentLocation.setText(FullAddress);
+        //Assistant show a dialog
+        assistant.setOnClickListener(v -> {
+            showAlertDialog2("Assistant", "call us or email us on ltaief.khalil@gmail.com for more assistance");
+        });
 
-
+    }
+    private void showAlertDialog2(String title, String message) {
+        // make a button open intent to call and another button to visit email and specify the email to send to ltaief.khalil@gmail.com .
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(title)
+                .setMessage(message)
+                .setPositiveButton("Call", (dialog, which) -> {
+                    android.content.Intent intent = new android.content.Intent(android.content.Intent.ACTION_DIAL);
+                    intent.setData(android.net.Uri.parse("tel:123456789"));
+                    startActivity(intent);
+                })
+                .setNegativeButton("Email", (dialog, which) -> {
+                    android.content.Intent intent = new android.content.Intent(android.content.Intent.ACTION_SEND);
+                    intent.setType("text/plain");
+                    intent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[]{"ltaief.khalil@gmail.com"});
+                    intent.putExtra(android.content.Intent.EXTRA_SUBJECT, "this custommer needs help");
+                    intent.putExtra(android.content.Intent.EXTRA_TEXT, "this custommer needs help"+ "\n" + "please contact him");
+                    startActivity(intent);
+                })
+                .show();
     }
     private void showAlertDialog(String title, String message) {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -109,9 +156,9 @@ public class AccountActivity extends AppCompatActivity {
                 .setPositiveButton("OK", (dialog, which) -> dialog.dismiss())
                 .show();
     }
+
     private boolean isValidInput() {
         return !TextUtils.isEmpty(username.getEditText().getText())
-                && !TextUtils.isEmpty(phone.getEditText().getText())
-                && !TextUtils.isEmpty(password.getEditText().getText());
+                && !TextUtils.isEmpty(phone.getEditText().getText());
     }
 }
--- app/src/main/java/com/example/renthub/Views/SplashScreenActivity.java
+++ app/src/main/java/com/example/renthub/Views/SplashScreenActivity.java
@@ -1,16 +1,15 @@
 package com.example.renthub.Views;
 
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import android.Manifest;
+
 import android.app.ActivityOptions;
-import android.content.pm.PackageManager;
-import android.location.Address;
-import android.location.Geocoder;
-import android.location.Location;
+
+import android.location.LocationListener;
 import android.os.Bundle;
+
 import android.util.Log;
 import android.util.Pair;
 import android.view.WindowManager;
@@ -21,29 +20,41 @@ import android.widget.TextView;
 
 import com.airbnb.lottie.LottieAnimationView;
 import com.example.renthub.R;
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+
+import android.location.LocationManager;
+
+import android.widget.Toast;
+
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
-public class SplashScreenActivity extends AppCompatActivity {
-    FusedLocationProviderClient fusedLocationProviderClient;
-    public static String lattitude;
-    public static String longitude;
+
+
+
+
+
+
+public class SplashScreenActivity extends AppCompatActivity implements LocationListener {
+    LocationManager locationManager;
+    private FirebaseAuth mAuth;
+    public static String lalltitude;
+    public static String longtitude;
     public static String address;
+    public static String area;
     public static String city;
     public static String country;
-    private final static int REQUEST_CODE = 100;
 
 
-
-    private FirebaseAuth mAuth;
     private static int SPLASH_SCREEN = 5000; // 5 seconds
 
     // variables
@@ -73,13 +84,79 @@ public class SplashScreenActivity extends AppCompatActivity {
 
 
        // coordination
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        //runtime permission
+        ActivityCompat.requestPermissions(this,new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},44);
+        getLocation();
+
+
+    }
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        Toast.makeText(this, ""+location.getLatitude()+"lang"+location.getLongitude(), Toast.LENGTH_SHORT).show();
+        try {
+            Geocoder geocoder = new Geocoder(SplashScreenActivity.this, Locale.getDefault());
+            List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+            lalltitude = String.valueOf(addresses.get(0).getLatitude());
+            longtitude = String.valueOf(addresses.get(0).getLongitude());
+            address = addresses.get(0).getAddressLine(0);
+            area = addresses.get(0).getLocality();
+            city = addresses.get(0).getAdminArea();
+            country = addresses.get(0).getCountryName();
+            Log.e("address", address);
+            Log.e("area", area);
+            Log.e("city", city);
+            Log.e("country", country);
+            Log.e("lalltitude", lalltitude);
+            Log.e("longtitude", longtitude);
+        } catch (IOException e) {
+            // Handle the exception here
+            Toast.makeText(this, "Error retrieving address information: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+
+        }
+
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull List<Location> locations) {
+        LocationListener.super.onLocationChanged(locations);
+    }
+
+    @Override
+    public void onFlushComplete(int requestCode) {
+        LocationListener.super.onFlushComplete(requestCode);
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        LocationListener.super.onStatusChanged(provider, status, extras);
+    }
+
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
+        LocationListener.super.onProviderEnabled(provider);
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
+        LocationListener.super.onProviderDisabled(provider);
+    }
+
+    public void getLocation(){
+        if(ActivityCompat.checkSelfPermission(SplashScreenActivity.this, android.Manifest.permission.ACCESS_FINE_LOCATION)==getPackageManager().PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(SplashScreenActivity.this, android.Manifest.permission.ACCESS_COARSE_LOCATION)==getPackageManager().PERMISSION_GRANTED){
+
+            locationManager=(LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,50000,5,SplashScreenActivity.this);
+        }
+        else{
+            ActivityCompat.requestPermissions(SplashScreenActivity.this,new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},44);
+
+        }
 
     }
     @Override
     public void onStart() {
         super.onStart();
-        getLastLocation();
+
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
         if(currentUser != null){
@@ -106,70 +183,5 @@ public class SplashScreenActivity extends AppCompatActivity {
             }, SPLASH_SCREEN);
         }
     }
-    private void getLastLocation(){
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
 
-
-            fusedLocationProviderClient.getLastLocation()
-                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                        @Override
-                        public void onSuccess(Location location) {
-                            if (location != null) {
-                                try {
-                                    Geocoder geocoder = new Geocoder(SplashScreenActivity.this, Locale.getDefault());
-                                    List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
-
-                                    if (addresses != null && addresses.size() > 0) {
-                                        lattitude = String.valueOf(addresses.get(0).getLatitude());
-                                        longitude = String.valueOf(addresses.get(0).getLongitude());
-                                        address = String.valueOf(addresses.get(0).getAddressLine(0));
-                                        city = String.valueOf(addresses.get(0).getLocality());
-                                        country = String.valueOf(addresses.get(0).getCountryName());
-
-                                        // Log the location details
-                                        Log.d("TAG", "Location: " + lattitude + ", " + longitude);
-                                        Log.d("TAG", "Address: " + address + ", " + city + ", " + country);
-                                    } else {
-                                        Log.d("TAG", "No address found");
-                                    }
-                                } catch (IOException e) {
-                                    e.printStackTrace();
-                                    Log.e("TAG", "IOException while getting location: " + e.getMessage());
-                                }
-                            } else {
-                                Log.d("TAG", "Location is null");
-                            }
-                        }
-                    })
-                    .addOnFailureListener(this, new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Log.e("TAG", "Error getting last location: " + e.getMessage());
-                        }
-                    });
-
-
-    }
-
-
-    else {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
-        }
-    }
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull @org.jetbrains.annotations.NotNull String[] permissions, @NonNull @org.jetbrains.annotations.NotNull int[] grantResults) {
-
-        if (requestCode == REQUEST_CODE){
-
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-
-
-                getLastLocation();
-
-            }
-
-        }
-
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-    }
 }   
