060ce2815b7eae47440d7df701cd1d5942f55ceb
jojo52807531 <97908088+jojo52807531@users.noreply.github.com>
2023-12-02 16:20:59 +0100

    home

--- app/src/main/java/com/example/renthub/Views/HomeActivity.java
+++ app/src/main/java/com/example/renthub/Views/HomeActivity.java
@@ -16,6 +16,7 @@ import java.util.List;
 import java.util.UUID;
 
 public class HomeActivity extends AppCompatActivity {
+
     private RecyclerView recyclerView;
     private StaticRvAdapter staticRvAdapter;
     List<DynamicRVModel> items =  new ArrayList();
@@ -26,45 +27,52 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         ArrayList<StaticRvModel> item = new ArrayList<>();
-        item.add(new StaticRvModel(R.drawable.car, "Popular"));
-        item.add(new StaticRvModel(R.drawable.car, "SUV"));
-        item.add(new StaticRvModel(R.drawable.car, "Limo"));
-        item.add(new StaticRvModel(R.drawable.car, "Truck"));
-        item.add(new StaticRvModel(R.drawable.car, "Luxury"));
-        item.add(new StaticRvModel(R.drawable.car, "Sports"));
+        item.add(new StaticRvModel(R.drawable.suv, "SUV"));
+        item.add(new StaticRvModel(R.drawable.convertible, "CONVERTIBLE"));
+        item.add(new StaticRvModel(R.drawable.coupe, "COUPE"));
+        item.add(new StaticRvModel(R.drawable.hatchback, "HATCHBACK"));
+        item.add(new StaticRvModel(R.drawable.sedan, "SEDAN"));
+        item.add(new StaticRvModel(R.drawable.sport, "Sports"));
+        item.add(new StaticRvModel(R.drawable.truck, "TRUCK"));
+        item.add(new StaticRvModel(R.drawable.van, "VAN"));
+        item.add(new StaticRvModel(R.drawable.pickup, "PICKUP"));
+        item.add(new StaticRvModel(R.drawable.mini_bus, "MINI_BUS"));
 
-
-        recyclerView= findViewById(R.id.rv_1);
+      recyclerView = findViewById(R.id.rv_1);
         staticRvAdapter = new StaticRvAdapter(item);
         recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
         recyclerView.setAdapter(staticRvAdapter);
 
-        items.add(new DynamicRVModel("Truck"));
-        items.add(new DynamicRVModel("SUV"));
+       /*   items.add(new DynamicRVModel("SUV"));
+        items.add(new DynamicRVModel("CONVERTIBLE"));
+        items.add(new DynamicRVModel("COUPE"));
+        items.add(new DynamicRVModel("HATCHBACK"));
+        items.add(new DynamicRVModel("SEDAN"));
         items.add(new DynamicRVModel("Sports"));
-        items.add(new DynamicRVModel("Limo"));
-        items.add(new DynamicRVModel("Luxury"));
-        items.add(new DynamicRVModel("Popular"));
+        items.add(new DynamicRVModel("TRUCK"));
+        items.add(new DynamicRVModel("VAN"));
+        items.add(new DynamicRVModel("PICKUP"));
+        items.add(new DynamicRVModel("MINI_BUS"));*/
 
         RecyclerView drv = findViewById(R.id.rv_2);
         drv.setLayoutManager(new LinearLayoutManager(this));
-        dynamicRVAdapter = new DynamicRVAdapter(drv, this,items);
+        dynamicRVAdapter = new DynamicRVAdapter(drv, this, items);
         drv.setAdapter(dynamicRVAdapter);
 
         dynamicRVAdapter.setLoadMore(new LoadMore() {
             @Override
             public void onLoadMore() {
-                if(items.size()<=10){
+                if (items.size() <= 10) {
                     item.add(null);
-                    dynamicRVAdapter.notifyItemInserted(items.size()-1);
+                    dynamicRVAdapter.notifyItemInserted(items.size() - 1);
                     new Handler().postDelayed(new Runnable() {
                         @Override
                         public void run() {
-                            items.remove(items.size()-1);
+                            items.remove(items.size() - 1);
                             dynamicRVAdapter.notifyItemInserted(items.size());
-                            int index  = items.size();
-                            int end = index+10;
-                            for (int i = index; i<end;i++){
+                            int index = items.size();
+                            int end = index + 10;
+                            for (int i = index; i < end; i++) {
                                 String name = UUID.randomUUID().toString();
                                 DynamicRVModel item = new DynamicRVModel(name);
                                 items.add(item);
@@ -72,17 +80,36 @@ public class HomeActivity extends AppCompatActivity {
                             dynamicRVAdapter.notifyDataSetChanged();
                             dynamicRVAdapter.setLoaded();
                         }
-                    },4000);
+                    }, 4000);
 
-                }
-                else
+                } else {
                     Toast.makeText(HomeActivity.this, "Data Completed", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
+        // Set up click listener for static RecyclerView items
+        staticRvAdapter.setOnItemClickListener(new StaticRvAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(StaticRvModel item) {
+                // Handle item click by adding it to the dynamic RecyclerView
+                addItemToDynamicRecyclerView(item);
+            }
+        });
+    }
 
+    private void addItemToDynamicRecyclerView(StaticRvModel staticRvModel) {
+        // Extract information from the clicked static item
+        String itemName = staticRvModel.getText();
+        int itemImage = staticRvModel.getImage();
 
+        // Create a DynamicRVModel with the extracted information
+        DynamicRVModel dynamicRVModel = new DynamicRVModel(itemName);
 
+        // Add the item to the dynamic RecyclerView's adapter
+        items.add(dynamicRVModel);
 
+        // Notify the adapter that the dataset has changed
+        dynamicRVAdapter.notifyDataSetChanged();
     }
 }
--- app/src/main/java/com/example/renthub/Views/StaticRvAdapter.java
+++ app/src/main/java/com/example/renthub/Views/StaticRvAdapter.java
@@ -14,21 +14,29 @@ import com.example.renthub.R;
 
 import java.util.ArrayList;
 
-
 public class StaticRvAdapter extends RecyclerView.Adapter<StaticRvAdapter.StaticRVViewHolder> {
     private ArrayList<StaticRvModel> items;
+    private OnItemClickListener onItemClickListener;
     int row_index = -1;
 
     public StaticRvAdapter(ArrayList<StaticRvModel> items) {
         this.items = items;
     }
 
+    // Interface for item click events
+    public interface OnItemClickListener {
+        void onItemClick(StaticRvModel item);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        this.onItemClickListener = listener;
+    }
+
     @NonNull
     @Override
     public StaticRVViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.static_rv_item,parent,false);
-        StaticRVViewHolder staticRVViewHolder = new StaticRVViewHolder(view);
-        return staticRVViewHolder;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.static_rv_item, parent, false);
+        return new StaticRVViewHolder(view);
     }
 
     @Override
@@ -41,33 +49,33 @@ public class StaticRvAdapter extends RecyclerView.Adapter<StaticRvAdapter.Static
             public void onClick(View v) {
                 row_index = position;
                 notifyDataSetChanged();
+                if (onItemClickListener != null) {
+                    onItemClickListener.onItemClick(currentItem);
+                }
             }
         });
-        if (row_index == position){
+        if (row_index == position) {
             holder.linearLayout.setBackgroundResource(R.drawable.static_rv_selected_bg);
-        }
-        else {
+        } else {
             holder.linearLayout.setBackgroundResource(R.drawable.static_rv_bg);
         }
     }
 
     @Override
     public int getItemCount() {
-        return  items.size();
+        return items.size();
     }
 
-    public static class StaticRVViewHolder extends RecyclerView.ViewHolder{
+    public static class StaticRVViewHolder extends RecyclerView.ViewHolder {
         TextView textView;
         ImageView imageView;
         LinearLayout linearLayout;
-
 
         public StaticRVViewHolder(@NonNull View itemView) {
             super(itemView);
             imageView = itemView.findViewById(R.id.image);
             textView = itemView.findViewById(R.id.text);
-            linearLayout= itemView.findViewById(R.id.linearLayout);
-
+            linearLayout = itemView.findViewById(R.id.linearLayout);
         }
     }
 }
